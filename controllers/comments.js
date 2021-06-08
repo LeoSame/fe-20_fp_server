aeed55162db6f2baed773668b0b04133df9bc948
Leonid Semeniuk <leo.sam@ukr.net>
2021-06-08 21:40:23 +0300

    add sort in comments controllers

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,24 +1,20 @@
-const Comment = require("../models/Comment");
-const queryCreator = require("../commonHelpers/queryCreator");
-const _ = require("lodash");
+const Comment = require('../models/Comment');
+const queryCreator = require('../commonHelpers/queryCreator');
+const _ = require('lodash');
 
 exports.addComment = (req, res, next) => {
   const commentData = _.cloneDeep(req.body);
   commentData.customer = req.user.id;
   const newComment = new Comment(queryCreator(commentData));
 
-  newComment
-    .populate("product")
-    .populate("category")
-    .populate("customer")
-    .execPopulate();
+  newComment.populate('product').populate('category').populate('customer').execPopulate();
 
   newComment
     .save()
     .then(comment => res.json(comment))
     .catch(err =>
       res.status(400).json({
-        message: `Error happened on server: "${err}" `
+        message: `Error happened on server: "${err}" `,
       })
     );
 };
@@ -28,31 +24,27 @@ exports.updateComment = (req, res, next) => {
     .then(comment => {
       if (!comment) {
         return res.status(400).json({
-          message: `Comment with _id "${req.params.id}" is not found.`
+          message: `Comment with _id "${req.params.id}" is not found.`,
         });
       } else {
         const commentData = _.cloneDeep(req.body);
         const updatedComment = queryCreator(commentData);
 
-        Comment.findOneAndUpdate(
-          { _id: req.params.id },
-          { $set: updatedComment },
-          { new: true }
-        )
-          .populate("product")
-          .populate("category")
-          .populate("customer")
+        Comment.findOneAndUpdate({ _id: req.params.id }, { $set: updatedComment }, { new: true })
+          .populate('product')
+          .populate('category')
+          .populate('customer')
           .then(comment => res.json(comment))
           .catch(err =>
             res.status(400).json({
-              message: `Error happened on server: "${err}" `
+              message: `Error happened on server: "${err}" `,
             })
           );
       }
     })
     .catch(err =>
       res.status(400).json({
-        message: `Error happened on server: "${err}" `
+        message: `Error happened on server: "${err}" `,
       })
     );
 };
@@ -61,23 +53,23 @@ exports.deleteComment = (req, res, next) => {
   Comment.findOne({ _id: req.params.id }).then(async comment => {
     if (!comment) {
       return res.status(400).json({
-        message: `Comment with id "${req.params.id}" is not found.`
+        message: `Comment with id "${req.params.id}" is not found.`,
       });
     } else {
       const commentToDelete = await Comment.findOne({
-        _id: req.params.id
+        _id: req.params.id,
       });
 
       Comment.deleteOne({ _id: req.params.id })
         .then(deletedCount =>
           res.status(200).json({
             message: `Comment witn id "${commentToDelete._id}" is successfully deletes from DB.`,
-            deletedCommentInfo: commentToDelete
+            deletedCommentInfo: commentToDelete,
           })
         )
         .catch(err =>
           res.status(400).json({
-            message: `Error happened on server: "${err}" `
+            message: `Error happened on server: "${err}" `,
           })
         );
     }
@@ -86,39 +78,40 @@ exports.deleteComment = (req, res, next) => {
 
 exports.getComments = (req, res, next) => {
   Comment.find()
-    .populate("product")
-    .populate("category")
-    .populate("customer")
+    .populate('product')
+    .populate('category')
+    .populate('customer')
     .then(comments => res.status(200).json(comments))
     .catch(err =>
       res.status(400).json({
-        message: `Error happened on server: "${err}" `
+        message: `Error happened on server: "${err}" `,
       })
     );
 };
 
 exports.getCustomerComments = (req, res, next) => {
   Comment.find({ customer: req.params.customerId })
-    .populate("product")
-    .populate("category")
-    .populate("customer")
+    .populate('product')
+    .populate('category')
+    .populate('customer')
     .then(comments => res.status(200).json(comments))
     .catch(err =>
       res.status(400).json({
-        message: `Error happened on server: "${err}" `
+        message: `Error happened on server: "${err}" `,
       })
     );
 };
 
 exports.getProductComments = (req, res, next) => {
   Comment.find({ product: req.params.productId })
-    .populate("product")
-    .populate("category")
-    .populate("customer")
+    .populate('product')
+    .populate('category')
+    .populate('customer')
+    .sort('-date')
     .then(comments => res.status(200).json(comments))
     .catch(err =>
       res.status(400).json({
-        message: `Error happened on server: "${err}" `
+        message: `Error happened on server: "${err}" `,
       })
     );
 };
