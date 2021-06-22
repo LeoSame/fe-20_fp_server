e638d00a260c80862a4d27c1a5c5a95c6d148cc0
Leonid Semeniuk <leo.sam@ukr.net>
2021-06-22 23:59:08 +0300

    fixed endpoint images

--- controllers/images.js
+++ controllers/images.js
@@ -1,22 +1,23 @@
 const Image = require('../models/Image');
 const path = require('path');
 const queryCreator = require('../commonHelpers/queryCreator');
+const { unlink } = require('fs');
 const _ = require('lodash');
 
 exports.addImage = (req, res, next) => {
+  let { name, affiliation } = req.body;
+  const { img } = req.files;
+
   try {
-    let { name, affiliation } = req.body;
-
-    const { img } = req.files;
-    let fileName = name + '.jpg';
-    let pathName = path.resolve(__dirname, '..', 'static', 'img', affiliation, fileName);
-
-    img.mv(pathName);
-
-    Image.findOne({ name: name + '.jpg' }).then(image => {
+    Image.findOne({ name: name }).then(image => {
       if (image) {
         return res.status(400).json({ message: `Картинка с названием "${image.name}" уже существует` });
       } else {
+        let fileName = name + '.jpg';
+        let pathName = `http://${req.headers.host}/static/img/${affiliation}/${fileName}`;
+
+        img.mv(path.resolve(__dirname, '..', 'static', 'img', affiliation, fileName));
+
         const initialQuery = _.cloneDeep({ ...req.body, imageUrl: pathName });
         const newImage = new Image(queryCreator(initialQuery));
 
@@ -37,59 +38,72 @@ exports.addImage = (req, res, next) => {
   }
 };
 
-// exports.updateBrand = (req, res, next) => {
-//   Brand.findOne({ _id: req.params.id })
-//     .then(brand => {
-//       if (!brand) {
-//         return res.status(400).json({ message: `Бренд с id "${req.params.id}" не найден.` });
-//       } else {
-//         const initialQuery = _.cloneDeep(req.body);
-//         const updatedBrand = queryCreator(initialQuery);
+exports.updateImage = (req, res, next) => {
+  Image.findOne({ name: req.params.name })
+    .then(image => {
+      if (!image) {
+        return res.status(400).json({ message: `Картинки с названием "${req.params.name}" нет в базе.` });
+      } else {
+        const initialQuery = _.cloneDeep(req.body);
+        const updatedImage = queryCreator(initialQuery);
 
-//         Brand.findOneAndUpdate({ _id: req.params.id }, { $set: updatedBrand }, { new: true })
-//           .then(brand => res.json(brand))
-//           .catch(err =>
-//             res.status(400).json({
-//               message: `Произошла ошибка на сервере: "${err}" `,
-//             })
-//           );
-//       }
-//     })
-//     .catch(err =>
-//       res.status(400).json({
-//         message: `Произошла ошибка на сервере: "${err}" `,
-//       })
-//     );
-// };
+        Image.findOneAndUpdate({ name: req.params.name }, { $set: updatedImage }, { new: true })
+          .then(image => res.json(image))
+          .catch(err =>
+            res.status(400).json({
+              message: `Произошла ошибка на сервере: "${err}" `,
+            })
+          );
+      }
+    })
+    .catch(err =>
+      res.status(400).json({
+        message: `Произошла ошибка на сервере: "${err}" `,
+      })
+    );
+};
 
-// exports.deleteBrand = (req, res, next) => {
-//   Brand.findOne({ _id: req.params.id }).then(async brand => {
-//     if (!brand) {
-//       return res.status(400).json({ message: `Бренд с id "${req.params.id}" не найден.` });
-//     } else {
-//       const brandToDelete = await Brand.findOne({ _id: req.params.id });
+exports.deleteImage = (req, res, next) => {
+  Image.findOne({ name: req.params.name }).then(async image => {
+    if (!image) {
+      return res.status(400).json({ message: `Картинка с названием "${req.params.name}" не найдена.` });
+    } else {
+      const imageToDelete = await Image.findOne({ name: req.params.name });
 
-//       Brand.deleteOne({ _id: req.params.id })
-//         .then(deletedCount =>
-//           res.status(200).json({
-//             message: `Бренд с названием "${brandToDelete.name}" успешно удален из БД `,
-//           })
-//         )
-//         .catch(err =>
-//           res.status(400).json({
-//             message: `Произошла ошибка на сервере: "${err}" `,
-//           })
-//         );
-//     }
-//   });
-// };
+      Image.deleteOne({ name: req.params.name })
+        .then(deletedCount => {
+          unlink(`static/img/${image.affiliation}/${image.name}.jpg`, function (err) {
+            if (err) return console.log(err);
+            console.log('file deleted successfully');
+          });
 
-// exports.getBrands = (req, res, next) => {
-//   Brand.find()
-//     .then(brands => res.json(brands))
-//     .catch(err =>
-//       res.status(400).json({
-//         message: `Произошла ошибка на сервере: "${err}" `,
-//       })
-//     );
-// };
+          res.status(200).json({
+            message: `Картинка с названием "${imageToDelete.name}" успешно удалена из БД `,
+          });
+        })
+        .catch(err =>
+          res.status(400).json({
+            message: `Произошла ошибка на сервере: "${err}" `,
+          })
+        );
+    }
+  });
+};
+
+exports.getImage = async (req, res, next) => {
+  const perPage = Number(req.query.perPage);
+  const startPage = Number(req.query.startPage);
+  try {
+    const images = await Image.find({ affiliation: req.query.affiliation })
+      .skip(startPage * perPage - perPage)
+      .limit(perPage);
+
+    const imagesQuantity = await Image.find({ affiliation: req.query.affiliation });
+
+    res.json({ images, imagesQuantity: imagesQuantity.length });
+  } catch (err) {
+    res.status(400).json({
+      message: `Произошла ошибка на сервере: "${err}" `,
+    });
+  }
+};
--- routes/images.js
+++ routes/images.js
@@ -3,26 +3,26 @@ const router = express.Router();
 const passport = require('passport'); // multer for parsing multipart form data (files)
 
 //Import controllers
-const { addImage } = require('../controllers/images');
+const { addImage, updateImage, deleteImage, getImage } = require('../controllers/images');
 
 // @route   POST /brands
 // @desc    Create new brand
 // @access  Private
 router.post('/', addImage);
 
-// // @route   PUT /brands/:id
-// // @desc    Update existing brand
-// // @access  Private
-// router.put('/:id', passport.authenticate('jwt-admin', { session: false }), updateBrand);
+// @route   PUT /brands/:id
+// @desc    Update existing brand
+// @access  Private
+router.put('/:name', updateImage);
 
-// // @route   DELETE /brands/:id
-// // @desc    DELETE existing brand
-// // @access  Private
-// router.delete('/:id', passport.authenticate('jwt-admin', { session: false }), deleteBrand);
+// @route   DELETE /brands/:id
+// @desc    DELETE existing brand
+// @access  Private
+router.delete('/:name', deleteImage);
 
-// // @route   GET /brands
-// // @desc    GET existing brand
-// // @access  Public
-// router.get('/', getBrands);
+// @route   GET /brands
+// @desc    GET existing brand
+// @access  Public
+router.get('/', getImage);
 
 module.exports = router;
